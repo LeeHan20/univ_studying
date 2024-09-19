dfec11655517931316d0ca05e2be4478a4dc5765
Han Lee <hi385790@gmail.com>
2024-09-19 13:50:19 +0900

    Update 3주차 : ??.java

--- 1학년 가을학기/프로그래밍설계방법론/3주차 : ??.java
+++ 1학년 가을학기/프로그래밍설계방법론/3주차 : ??.java
@@ -1,4 +1,20 @@
 //프로그래밍설계방법론 3주차
 
 public class Hello {
+  public static int sum(int n, int m) {
+    return n + m;
+  }
+  
+  public static void main(String[] args) { 
+    int i = 20;
+    int j = s;
+    char a;
+
+    s = sum(i, 10);
+    a = '?'
+    System.out.println(a);
+    System.out.println("Hello");
+    System.out.println(s);
 }
+
+//식별자 (변수하고 비슷함)
